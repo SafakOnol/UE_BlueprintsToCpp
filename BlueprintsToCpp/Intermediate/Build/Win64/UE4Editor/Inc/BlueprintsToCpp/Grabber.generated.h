a95b0741ae78c0b2d00dee153f20953475ee9385
SafakOnol <onolsafak@outlook.com>
2022-10-19 20:44:35 -0400

    Create cpp base classes, parent cpp classes to BP classes

---
+++ BlueprintsToCpp/Intermediate/Build/Win64/UE4Editor/Inc/BlueprintsToCpp/Grabber.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BLUEPRINTSTOCPP_Grabber_generated_h
+#error "Grabber.generated.h already included, missing '#pragma once' in Grabber.h"
+#endif
+#define BLUEPRINTSTOCPP_Grabber_generated_h
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_SPARSE_DATA
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_RPC_WRAPPERS
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGrabber(); \
+	friend struct Z_Construct_UClass_UGrabber_Statics; \
+public: \
+	DECLARE_CLASS(UGrabber, USceneComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlueprintsToCpp"), NO_API) \
+	DECLARE_SERIALIZER(UGrabber)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUGrabber(); \
+	friend struct Z_Construct_UClass_UGrabber_Statics; \
+public: \
+	DECLARE_CLASS(UGrabber, USceneComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlueprintsToCpp"), NO_API) \
+	DECLARE_SERIALIZER(UGrabber)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGrabber(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGrabber) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGrabber); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGrabber); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGrabber(UGrabber&&); \
+	NO_API UGrabber(const UGrabber&); \
+public:
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGrabber(UGrabber&&); \
+	NO_API UGrabber(const UGrabber&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGrabber); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGrabber); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UGrabber)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_PRIVATE_PROPERTY_OFFSET
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_10_PROLOG
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_PRIVATE_PROPERTY_OFFSET \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_SPARSE_DATA \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_RPC_WRAPPERS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_INCLASS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_PRIVATE_PROPERTY_OFFSET \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_SPARSE_DATA \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_INCLASS_NO_PURE_DECLS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BLUEPRINTSTOCPP_API UClass* StaticClass<class UGrabber>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BlueprintsToCpp_Source_BlueprintsToCpp_Grabber_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ BlueprintsToCpp/Intermediate/Build/Win64/UE4Editor/Inc/BlueprintsToCpp/QuestManager.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BLUEPRINTSTOCPP_QuestManager_generated_h
+#error "QuestManager.generated.h already included, missing '#pragma once' in QuestManager.h"
+#endif
+#define BLUEPRINTSTOCPP_QuestManager_generated_h
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_SPARSE_DATA
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_RPC_WRAPPERS
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAQuestManager(); \
+	friend struct Z_Construct_UClass_AQuestManager_Statics; \
+public: \
+	DECLARE_CLASS(AQuestManager, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlueprintsToCpp"), NO_API) \
+	DECLARE_SERIALIZER(AQuestManager)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAQuestManager(); \
+	friend struct Z_Construct_UClass_AQuestManager_Statics; \
+public: \
+	DECLARE_CLASS(AQuestManager, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlueprintsToCpp"), NO_API) \
+	DECLARE_SERIALIZER(AQuestManager)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AQuestManager(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AQuestManager) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AQuestManager); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AQuestManager); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AQuestManager(AQuestManager&&); \
+	NO_API AQuestManager(const AQuestManager&); \
+public:
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AQuestManager(AQuestManager&&); \
+	NO_API AQuestManager(const AQuestManager&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AQuestManager); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AQuestManager); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AQuestManager)
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_PRIVATE_PROPERTY_OFFSET
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_9_PROLOG
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_PRIVATE_PROPERTY_OFFSET \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_SPARSE_DATA \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_RPC_WRAPPERS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_INCLASS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_PRIVATE_PROPERTY_OFFSET \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_SPARSE_DATA \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_INCLASS_NO_PURE_DECLS \
+	BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BLUEPRINTSTOCPP_API UClass* StaticClass<class AQuestManager>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BlueprintsToCpp_Source_BlueprintsToCpp_Quests_QuestManager_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
